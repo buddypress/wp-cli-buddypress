385241e7ea54f029d9aaca4fd230dbdbc3d0dbd1
Renato Alves <espellcaste@gmail.com>
2017-10-10 18:03:52 -0300

    Groups: Added support for wp bp group demote

--- components/group.php
+++ components/group.php
@@ -472,6 +472,61 @@ class BPCLI_Group extends BPCLI_Component {
 	}
 
 	/**
+	 * Demote user to member.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <group-id>
+	 * : Identifier for the group. Accepts either a slug or a numeric ID.
+	 *
+	 * --user-id=<user>
+	 * : Identifier for the user. Accepts either a user_login or a numeric ID.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *    wp bp group demote --group-id=3 --user-id=10
+	 *    wp bp group demote --group-id=foo --user-id=admin
+	 *
+	 * @synopsis [--group-id=<group-id>] [--user-id=<user-id>]
+	 *
+	 * @since 1.3.0
+	 */
+	public function demote( $args, $assoc_args ) {
+		$r = wp_parse_args( $assoc_args, array(
+			'group-id' => '',
+			'user-id'  => '',
+		) );
+
+		// Convert --group_id to group ID
+		// @todo this'll be screwed up if the group has a numeric slug.
+		$group_id = ( ! is_numeric( $r['group-id'] ) )
+			? groups_get_id( $r['group-id'] )
+			: $r['group-id'];
+
+		// Check that group exists.
+		$group_obj = groups_get_group( array(
+			'group_id' => $group_id,
+		) );
+		if ( empty( $group_obj->id ) ) {
+			WP_CLI::error( 'No group found by that slug or ID.' );
+		}
+
+		$user = $this->get_user_id_from_identifier( $r['user-id'] );
+
+		if ( ! $user ) {
+			WP_CLI::error( 'No user found by that username or ID' );
+		}
+
+		$member = new BP_Groups_Member( $user->ID, $group_id );
+
+		if ( $member->demote() ) {
+			WP_CLI::success( sprintf( 'User demoted to user.' ) );
+		} else {
+			WP_CLI::error( 'Could not demote the user.' );
+		}
+	}
+
+	/**
 	 * Group Roles
 	 *
 	 * @since 1.3.0
