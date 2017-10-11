01bf37cb450afe8cdefde4cb7c3c881e7bc5c56c
Renato Alves <espellcaste@gmail.com>
2017-10-11 20:40:15 -0300

    Groups: Added support for wp bp group send_invites

--- components/group.php
+++ components/group.php
@@ -986,6 +986,53 @@ class BPCLI_Group extends BPCLI_Component {
 	}
 
 	/**
+	 * Send pending invites by a user to a group.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--group-id=<group-id>]
+	 * : Identifier for the group. Accepts either a slug or a numeric ID.
+	 *
+	 * [--user-id=<user>]
+	 * : Identifier for the user. Accepts either a user_login or a numeric ID.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *    wp bp group send_invites --group-id=3 --user-id=10
+	 *    wp bp group send_invites --group-id=foo --user-id=admin
+	 *
+	 * @synopsis [--group-id=<group-id>] [--user-id=<user-id>]
+	 *
+	 * @since 1.3.0
+	 */
+	public function send_invites( $args, $assoc_args ) {
+		$r = wp_parse_args( $assoc_args, array(
+			'group-id'      => '',
+			'user-id'       => '',
+		) );
+
+		// Group ID.
+		$group_id = $r['group-id'];
+
+		// Check that group exists.
+		if ( ! $this->group_exists( $group_id ) ) {
+			WP_CLI::error( 'No group found by that slug or ID.' );
+		}
+
+		$user = $this->get_user_id_from_identifier( $r['user-id'] );
+
+		if ( ! $user ) {
+			WP_CLI::error( 'No user found by that username or ID' );
+		}
+
+		if ( groups_send_invites( $user->ID, $group_id ) ) {
+			WP_CLI::success( 'Invitations by the user sent.' );
+		} else {
+			WP_CLI::error( 'Could not send the invitations.' );
+		}
+	}
+
+	/**
 	 * Group Roles
 	 *
 	 * @since 1.3.0
