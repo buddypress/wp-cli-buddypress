e2b7691087d16b5314073cc84fe1e891135fa822
Renato Alves <espellcaste@gmail.com>
2018-01-06 18:25:33 -0300

    Core: Missing point

--- components/core.php
+++ components/core.php
@@ -159,7 +159,7 @@ class BPCLI_Core extends BPCLI_Component {
 		}
 
 		if ( 'count' === $formatter->format ) {
-			$formatter->display_items( $current_components )
+			$formatter->display_items( $current_components );
 		} else {
 			$formatter->display_items( $current_components );
 		}
