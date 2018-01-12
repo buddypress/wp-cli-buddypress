91964bc1eb416ff449625ba7c6b2d3556359b829
Renato Alves <espellcaste@gmail.com>
2018-01-12 00:23:25 -0300

    Friends: wp bp friend create command

--- components/friend.php
+++ components/friend.php
@@ -5,6 +5,62 @@
  * @since 1.6.0
  */
 class BPCLI_Friend extends BPCLI_Component {
+
+	/**
+	 * Create a new friendship.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <initiator>
+	 * : ID of the user who is sending the friendship request. Accepts either a user_login or a numeric ID.
+	 *
+	 * <friend>
+	 * : ID of the user whose friendship is being requested. Accepts either a user_login or a numeric ID.
+	 *
+	 * [force-accept=<force-accept>]
+	 * : Whether to force acceptance.
+	 * ---
+	 * default: true
+	 * ---
+	 *
+	 * [--silent=<silent>]
+	 * : Silent friendship creation.
+	 * ---
+	 * Default: false
+	 * ---
+	 *
+	 * ## EXAMPLE
+	 *
+	 *     $ wp bp friend create user1 another_use
+	 *     Success: Friendship successfully created.
+	 *
+	 *     $ wp bp friend create user1 another_use force-accept
+	 *     Success: Friendship successfully created.
+	 *
+	 * @alias add
+	 */
+	public function create( $args, $assoc_args ) {
+		// Members.
+		$initiator = $this->get_user_id_from_identifier( $args[0] );
+		$friend = $this->get_user_id_from_identifier( $args[1] );
+
+		if ( ! $initiator || ! $friend ) {
+			WP_CLI::error( 'No user found by that username or ID.' );
+		}
+
+		$force_accept = ( (bool) $assoc_args['force-accept'] ) ? false : true;
+
+		if ( friends_add_friend( $initiator->ID, $friend->ID, $force_accept ) ) {
+
+			if ( $assoc_args['silent'] ) {
+				return;
+			}
+
+			WP_CLI::success( 'Friendship successfully created.' );
+		} else {
+			WP_CLI::error( 'There was a problem while creating the friendship.' );
+		}
+	}
 }
 
 WP_CLI::add_command( 'bp friend', 'BPCLI_Friend', array(
--- features/friend.feature
+++ features/friend.feature
@@ -2,3 +2,17 @@ Feature: Manage BuddyPress Friends
 
   Scenario: Friends CRUD Operations
     Given a BP install
+
+    When I run `wp user create testuser1 testuser1@example.com --porcelain`
+    Then STDOUT should be a number
+    And save STDOUT as {MEMBER_ID}
+
+    When I run `wp user create testuser2 testuser2@example.com --porcelain`
+    Then STDOUT should be a number
+    And save STDOUT as {MEMBER_ID_2}
+
+    When I run `wp bp friend create {MEMBER_ID} {MEMBER_ID_2}`
+    Then STDOUT should contain:
+      """
+      Success: Friendship successfully created.
+      """
