382cd27c583115fb1e23d826138cfc62015be2df
r-a-y <r-a-y@gmx.com>
2018-02-14 08:29:01 -0800

    Email: For 'create' sub-command, add missing documentation for 'file' argument and 'edit' option.
    Also includes an example.

--- components/email.php
+++ components/email.php
@@ -25,9 +25,27 @@ class BPCLI_Email extends BPCLI_Component {
 	 * [--plain-text-content=<plain-text-content>]
 	 * : Plain-text email content. Email tokens allowed. View https://codex.buddypress.org/emails/email-tokens/ for more info.
 	 *
+	 * [<file>]
+	 * : Read content from <file>. If this value is present, the
+	 *     `--content` argument will be ignored.
+	 *
+	 *   Passing `-` as the filename will cause post content to
+	 *   be read from STDIN.
+	 *
+	 * [--edit]
+	 * : Immediately open system's editor to write or edit email content.
+	 *
+	 *   If content is read from a file, from STDIN, or from the `--content`
+	 *   argument, that text will be loaded into the editor.
+	 *
 	 * ## EXAMPLES
 	 *
+	 *     # Create email post
 	 *     $ wp bp email create --type=new-event --type-description="Send an email when a new event is created" --subject="[{{{site.name}}}] A new event was created" --content="<a href='{{{some.custom-token-url}}}'></a>A new event</a> was created" --plain-text-content="A new event was created"
+	 *     Success: Email post created for type "new-event".
+	 *
+	 *     # Create email post with content from given file
+	 *     $ wp bp email create ./email-content.txt --type=new-event --type-description="Send an email when a new event is created" --subject="[{{{site.name}}}] A new event was created" --plain-text-content="A new event was created"
 	 *     Success: Email post created for type "new-event".
 	 *
 	 * @alias add
